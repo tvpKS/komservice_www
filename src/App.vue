f5805a8f3397e2e5cf08d2527a483c7fd8117a8b
catastrophico <37561729+catastrophico@users.noreply.github.com>
2020-06-01 14:05:21 +0300

    styling and align footer

--- src/App.vue
+++ src/App.vue
@@ -25,15 +25,15 @@
 
     <router-view/>
 
-    <div id="footer">
-        <div>
-          <ul class="footer_link">
+    <div class="footer">
+          <ul id="footer_menu">
             <li><a class="scq" href="http://komservice.net/#sc0">Головна</a></li>
             <li><a class="scw" href="http://komservice.net/#sc2">Абоненту</a></li>
             <li><a class="sce" href="http://komservice.net/#sc3">Тарифи</a></li>
             <li><a class="scr" href="http://komservice.net/#sc4">Телебачення</a></li>
             <li><a class="sct" href="http://komservice.net/#sc5">Контакти</a></li>
-
+          </ul>
+         <ul id="footer_menu_2" class="link_color">
             <li><a href="http://info.komservice.net/cgi-bin/stat.pl">Сторінка статистики</a></li>
             <li><a href="http://komservice.net/?idget=101">Оплата послуг</a></li>
             <li><a href="http://komservice.net/?idget=102">Пристрої та матеріали</a></li>
@@ -41,7 +41,6 @@
             <li><a href="http://komservice.net/?idget=104">Акції</a></li>
             <li><a href="http://komservice.net/?idget=105">Договір оферти</a></li>
           </ul>
-        </div>
         <div class="soc">
           <a class="img_fb" href="https://www.facebook.com/komserviceovruch"><img src="img/FB.png" alt=""></a>
           <a class="img_tw" href="https://www.twitter.com/komservice"><img src="img/Twitter.png" alt=""></a>
